187fd2bdf400ffe39d1b3bf207a91180ac065aed
Sean Sube <seansube@gmail.com>
2025-12-05 13:10:54 -0600

    fix bug where non-flammable items were being burned

--- scripts/observers/ZO_Fire.gd
+++ scripts/observers/ZO_Fire.gd
@@ -7,6 +7,9 @@ func watch() -> Resource:
 
 func on_component_added(entity: Entity, _component: Resource) -> void:
 	var flammable = entity.get_component(ZC_Flammable) as ZC_Flammable
+	if flammable == null:
+		return
+
 	var effect_node = entity.get_node(flammable.effect_path) as Node3D
 	if effect_node != null:
 		effect_node.visible = true
@@ -14,6 +17,9 @@ func on_component_added(entity: Entity, _component: Resource) -> void:
 
 func on_component_removed(entity: Entity, _component: Resource):
 	var flammable = entity.get_component(ZC_Flammable) as ZC_Flammable
+	if flammable == null:
+		return
+
 	var effect_node = entity.get_node(flammable.effect_path) as Node3D
 	if effect_node != null:
 		effect_node.visible = false
