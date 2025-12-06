4dc0710afe54d5ab502c916628dfc81a55415a3c
Sean Sube <seansube@gmail.com>
2025-12-06 11:33:09 -0600

    fix indentation in mouse script

--- scripts/nodes/ZN_Mouse.gd
+++ scripts/nodes/ZN_Mouse.gd
@@ -30,49 +30,49 @@ class_name ZN_MouseInput
 
 
 func _ready():
-    Input.set_use_accumulated_input(false)
+	Input.set_use_accumulated_input(false)
 
 
 func _unhandled_input(event) -> void:
-    if event is InputEventMouseMotion:
-        aim_look(event)
+	if event is InputEventMouseMotion:
+		aim_look(event)
 
 
 ## Handles aim look with the mouse.
 func aim_look(event: InputEventMouseMotion) -> void:
-    var viewport_transform: Transform2D = get_tree().root.get_final_transform()
-    var motion: Vector2 = event.xformed_by(viewport_transform).relative
+	var viewport_transform: Transform2D = get_tree().root.get_final_transform()
+	var motion: Vector2 = event.xformed_by(viewport_transform).relative
 
-    motion *= mouse_sensitivity
-    motion *= degrees_per_unit
+	motion *= mouse_sensitivity
+	motion *= degrees_per_unit
 
-    add_yaw(motion.x)
-    add_pitch(motion.y)
-    clamp_pitch()
+	add_yaw(motion.x)
+	add_pitch(motion.y)
+	clamp_pitch()
 
 
 ## Rotates the character around the local Y axis by a given amount (in degrees) to achieve yaw.
 func add_yaw(amount) -> void:
-    if is_zero_approx(amount):
-        return
+	if is_zero_approx(amount):
+		return
 
-    character.rotate_object_local(Vector3.DOWN, deg_to_rad(amount))
-    character.orthonormalize()
+	character.rotate_object_local(Vector3.DOWN, deg_to_rad(amount))
+	character.orthonormalize()
 
 
 ## Rotates the head around the local x axis by a given amount (in degrees) to achieve pitch.
 func add_pitch(amount) -> void:
-    if is_zero_approx(amount):
-        return
+	if is_zero_approx(amount):
+		return
 
-    head.rotate_object_local(Vector3.LEFT, deg_to_rad(amount))
-    head.orthonormalize()
+	head.rotate_object_local(Vector3.LEFT, deg_to_rad(amount))
+	head.orthonormalize()
 
 
 ## Clamps the pitch between min_pitch and max_pitch.
 func clamp_pitch() -> void:
-    if head.rotation.x > deg_to_rad(min_pitch) and head.rotation.x < deg_to_rad(max_pitch):
-        return
+	if head.rotation.x > deg_to_rad(min_pitch) and head.rotation.x < deg_to_rad(max_pitch):
+		return
 
-    head.rotation.x = clamp(head.rotation.x, deg_to_rad(min_pitch), deg_to_rad(max_pitch))
-    head.orthonormalize()
+	head.rotation.x = clamp(head.rotation.x, deg_to_rad(min_pitch), deg_to_rad(max_pitch))
+	head.orthonormalize()
