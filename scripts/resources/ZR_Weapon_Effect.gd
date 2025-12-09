bd553cacc3a01d0609ce402eb28ef6707276e683
Sean Sube <seansube@gmail.com>
2025-12-08 18:13:56 -0600

    add melee swing weapon effect type

--- scripts/resources/ZR_Weapon_Effect.gd
+++ scripts/resources/ZR_Weapon_Effect.gd
@@ -1,13 +1,13 @@
 extends Resource
 class_name ZR_Weapon_Effect
 
-enum EffectType { CODE_ONLY = 0, MUZZLE_FIRE = 1, RECOIL = 2, RELOAD = 4 }
+enum EffectType { CODE_ONLY, MELEE_SWING, MUZZLE_FIRE, RECOIL, RELOAD }
 
 @export var effect_type: EffectType = EffectType.CODE_ONLY
 @export var marker: NodePath
 @export var effect_scene: PackedScene
 
-## TODO: could contain the actual bullet for MUZZLE_FIRE
+## TODO: can be removed in favor of the projectile for the MUZZLE_FIRE event
 @export var projectile_scene: PackedScene
 
 ## TODO: unused
