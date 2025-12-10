a8420be647b884c4b7af66ff15ec835c2eb47ed8
Sean Sube <seansube@gmail.com>
2025-12-09 18:51:28 -0600

    allow copy components to take a list of entities

--- scripts/nodes/ZN_CopyComponents.gd
+++ scripts/nodes/ZN_CopyComponents.gd
@@ -1,10 +1,11 @@
 extends Node3D
 
-@export var child_entity: Entity
+@export var entities: Array[Entity] = []
 @export var components: Array[Component] = []
 
 func _ready() -> void:
-	assert(child_entity != null, "Child entity must be set to copy components!")
+	assert(entities.size() > 0, "Child entity must be set to copy components!")
 
-	for component in components:
-		child_entity.component_resources.append(component)
+	for entity in entities:
+		for component in components:
+			entity.component_resources.append(component)
