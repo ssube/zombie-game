d5e7ad327eb81b22766ced2f13b853f274462062
Sean Sube <seansube@gmail.com>
2025-12-14 18:25:07 -0600

    change trigger timer defaults to start on ready and one shot

--- scripts/nodes/ZN_TriggerTimer.gd
+++ scripts/nodes/ZN_TriggerTimer.gd
@@ -1,14 +1,23 @@
 extends Timer
 class_name ZN_TriggerTimer
 
+
+@export var start_on_ready: bool = true
+@export var repeat: bool = false
+
+
 var _actions: Array[ZN_BaseAction] = []
 
 func _ready() -> void:
+	self.one_shot = not repeat
 	timeout.connect(on_timeout)
 
 	for child in self.get_children():
 		if child is ZN_BaseAction:
 			_actions.append(child)
+
+	if start_on_ready:
+		self.start()
 
 
 func on_timeout() -> void:
