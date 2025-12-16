4221a5efc53ebae3d808fd11cae2dfa70a392776
Sean Sube <seansube@gmail.com>
2025-12-15 19:47:25 -0600

    start extracting enums into their own file

---
+++ scripts/utils/Enums.gd
@@ -0,0 +1,19 @@
+class_name Enums
+
+enum ActionEvent {
+	AREA_INTERVAL = 0,
+	BODY_ENTER = 10,
+	BODY_EXIT = 11,
+	BODY_INTERVAL = 12,
+	BUTTON_PRESS = 20,
+	BUTTON_RELEASE = 21,
+	ENTITY_PICKUP = 30,
+	ENTITY_USE = 31,
+	TIMER_TIMEOUT = 40,
+}
+
+enum Tristate {
+	UNSET = -1,
+	FALSE = 0,
+	TRUE = 1,
+}
