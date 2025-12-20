b44d1676e7a4b9a28b27e30746a40985ccb1c202
Sean Sube <seansube@gmail.com>
2025-12-20 13:56:01 -0600

    clean up screen effect component

--- scripts/components/effects/ZC_Screen_Effect.gd
+++ scripts/components/effects/ZC_Screen_Effect.gd
@@ -4,11 +4,11 @@ class_name ZC_Screen_Effect
 @export var effect: ZM_BaseMenu.Effects
 @export var duration: float = 1.0
 @export var strength: float = 1.0
-@export var total_duration: float = 1.0
+@export var curve_duration: float = 1.0
 @export var strength_curve: Curve
 
 func duration_ratio() -> float:
-	return duration / total_duration
+	return duration / curve_duration
 
 func current_strength() -> float:
 	if not strength_curve:
@@ -16,7 +16,3 @@ func current_strength() -> float:
 
 	var ratio := duration_ratio()
 	return strength_curve.sample(ratio)
-
-func _init() -> void:
-	# TODO: ends up using a lower value, usually the default
-	total_duration = duration
