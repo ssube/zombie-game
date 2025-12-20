1dab94635726f9cf20de5e44936781bd559bd2d0
Sean Sube <seansube@gmail.com>
2025-12-19 20:09:42 -0600

    make area action work with any kind of area

--- scripts/nodes/actions/ZN_AreaAction.gd
+++ scripts/nodes/actions/ZN_AreaAction.gd
@@ -1,7 +1,7 @@
 extends ZN_BaseAction
 class_name ZN_AreaAction
 
-@export var area: ZN_TriggerArea3D = null
+@export var area: Area3D = null
 @export var active: Enums.Tristate = Enums.Tristate.UNSET
 
 func run_entity(_source: Node, _event: Enums.ActionEvent, _actor: Entity) -> void:
@@ -9,7 +9,11 @@ func run_entity(_source: Node, _event: Enums.ActionEvent, _actor: Entity) -> voi
 		Enums.Tristate.UNSET:
 			pass
 		Enums.Tristate.FALSE:
-			area.active = false
+			area.monitoring = false
+			if area is ZN_TriggerArea3D:
+				area.active = false
 		Enums.Tristate.TRUE:
-			area.active = true
+			area.monitoring = true
+			if area is ZN_TriggerArea3D:
+				area.active = true
 		# TODO: add toggle
