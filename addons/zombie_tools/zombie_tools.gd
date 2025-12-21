a92977263a543ef51c9231d34851a15cbba02c9e
Sean Sube <seansube@gmail.com>
2025-12-21 11:00:26 -0600

    add zombie game-specific editor tools

---
+++ addons/zombie_tools/zombie_tools.gd
@@ -0,0 +1,242 @@
+@tool
+extends EditorPlugin
+
+
+func _enable_plugin() -> void:
+	# Add autoloads here.
+	pass
+
+
+func _disable_plugin() -> void:
+	# Remove autoloads here.
+	pass
+
+var check_lock_keys_button: Button
+var check_objective_keys_button: Button
+var fix_mesh_button: Button
+var sort_components_button: Button
+
+func _enter_tree():
+	fix_mesh_button = Button.new()
+	fix_mesh_button.text = "Fix Collision Mesh"
+	fix_mesh_button.pressed.connect(fix_collision_mesh)
+	add_control_to_container(CONTAINER_SPATIAL_EDITOR_MENU, fix_mesh_button)
+
+	sort_components_button = Button.new()
+	sort_components_button.text = "Sort Components"
+	sort_components_button.pressed.connect(sort_components)
+	add_control_to_container(CONTAINER_INSPECTOR_BOTTOM, sort_components_button)
+
+	check_lock_keys_button = Button.new()
+	check_lock_keys_button.text = "Check Lock Keys"
+	check_lock_keys_button.pressed.connect(check_lock_keys)
+	add_control_to_container(CONTAINER_INSPECTOR_BOTTOM, check_lock_keys_button)
+
+	check_objective_keys_button = Button.new()
+	check_objective_keys_button.text = "Check Objective Keys"
+	check_objective_keys_button.pressed.connect(check_objective_keys)
+	add_control_to_container(CONTAINER_INSPECTOR_BOTTOM, check_objective_keys_button)
+
+
+func _exit_tree():
+	remove_control_from_container(CONTAINER_SPATIAL_EDITOR_MENU, fix_mesh_button)
+	remove_control_from_container(CONTAINER_INSPECTOR_BOTTOM, sort_components_button)
+	remove_control_from_container(CONTAINER_INSPECTOR_BOTTOM, check_lock_keys_button)
+	remove_control_from_container(CONTAINER_INSPECTOR_BOTTOM, check_objective_keys_button)
+	fix_mesh_button.queue_free()
+
+
+func fix_collision_mesh() -> void:
+	var selected = get_editor_interface().get_selection().get_selected_nodes()
+	print("Checking meshes: ", selected)
+
+	for selection in selected:
+		if selection is CollisionShape3D:
+			var shape = selection.shape
+			if shape is ConvexPolygonShape3D: #  or shape is ConcavePolygonShape3D:
+				var scale = selection.scale
+				print("Fixing: ", shape, " with scale: ", scale)
+
+				var points = shape.points.duplicate()
+				var i = 0
+				while i < points.size():
+					var point = points.get(i)
+					point *= scale
+					points.set(i, point)
+					i += 1
+
+				print("Fixed %d points in shape" % i)
+				shape.points = points
+				selection.scale = Vector3.ONE
+			else:
+				print("Cannot fix shape type: ", shape)
+
+
+func _sort_component_class(a: Component, b: Component) -> int:
+	var a_script := a.get_script() as Script
+	var b_script := b.get_script() as Script
+	var a_name := a_script.get_global_name()
+	var b_name := b_script.get_global_name()
+
+	return b_name < a_name
+
+
+func sort_components() -> void:
+	var selected = get_editor_interface().get_selection().get_selected_nodes()
+	print("Sorting components: ", selected)
+
+	for selection in selected:
+		if selection is Entity:
+			selection.component_resources.sort_custom(_sort_component_class)
+			print("Sorted %d components." % selection.component_resources.size())
+
+		if selection is ZE_Base:
+			selection.extra_components.sort_custom(_sort_component_class)
+			print("Sorted %d extra components." % selection.extra_components.size())
+
+	notify_property_list_changed()
+
+
+func _find_child_entities(node: Node) -> Array[Entity]:
+	var entities: Array[Entity] = []
+	for child in node.get_children():
+		if child is Entity:
+			entities.append(child)
+
+		var child_entities := _find_child_entities(child)
+		entities.append_array(child_entities)
+
+	return entities
+
+
+func _find_child_objectives(node: Node) -> Array[ZN_BaseObjective]:
+	var objectives: Array[ZN_BaseObjective] = []
+	for child in node.get_children():
+		if child is ZN_BaseObjective:
+			objectives.append(child)
+
+		var child_objectives := _find_child_objectives(child)
+		objectives.append_array(child_objectives)
+
+	return objectives
+
+
+func _get_key_component(entity: Node) -> Component:
+	# Check extras first because they override the resources
+	for component in entity.extra_components:
+		if component is ZC_Key:
+			return component
+
+	for component in entity.component_resources:
+		if component is ZC_Key:
+			return component
+
+	return null
+
+
+func _get_locked_component(entity: Node) -> Component:
+	# Check extras first because they override the resources
+	for component in entity.extra_components:
+		if component is ZC_Locked:
+			return component
+
+	for component in entity.component_resources:
+		if component is ZC_Locked:
+			return component
+
+	return null
+
+
+func _get_objective_component(entity: Node) -> Component:
+	# Check extras first because they override the resources
+	for component in entity.extra_components:
+		if component is ZC_Objective:
+			return component
+
+	for component in entity.component_resources:
+		if component is ZC_Objective:
+			return component
+
+	return null
+
+
+func check_lock_keys() -> void:
+	# Find locked entities in the current scene and their corresponding keys
+	var editor_interface := get_editor_interface()
+	var scene_root := editor_interface.get_edited_scene_root() as Node3D
+	if not scene_root:
+		return
+
+	#print("Current scene root: ", scene_root.name)
+	var scene_entities := _find_child_entities(scene_root)
+
+	var key_names: Array[String] = []
+	var lock_keys: Array[String] = []
+
+	for entity in scene_entities:
+		#print("Checking entity: %s" % entity.name)
+		var key := _get_key_component(entity) as ZC_Key
+		var locked := _get_locked_component(entity) as ZC_Locked
+
+		if key:
+			#print("Found key: %s" % key.name)
+			key_names.append(key.name)
+
+		if locked:
+			#print("Found locked: %s" % locked.key_name)
+			lock_keys.append(locked.key_name)
+
+	# Compare locks to keys
+	var missing := 0
+	for key_name in lock_keys:
+		if key_name in key_names:
+			print("Found key %s in the level." % key_name)
+		else:
+			# TODO: find a way to skip keys that are given in earlier levels
+			printerr("Key %s does not exist in the level!" % key_name)
+			missing += 1
+
+	if missing == 0:
+		print("Level has all keys.")
+	else:
+		printerr("Level is missing %d keys!" % missing)
+
+
+func check_objective_keys() -> void:
+	# Find objectives in the current scene and their corresponding keys
+	var editor_interface := get_editor_interface()
+	var scene_root := editor_interface.get_edited_scene_root() as Node3D
+	if not scene_root:
+		return
+
+	#print("Current scene root: ", scene_root.name)
+	var scene_entities := _find_child_entities(scene_root)
+	var scene_objectives := _find_child_objectives(scene_root)
+
+	var objective_names: Array[String] = []
+
+	for objective in scene_objectives:
+		print("Found objective: %s" % objective.key)
+		objective_names.append(objective.key)
+
+	var missing := 0
+	for entity in scene_entities:
+		var objective := _get_objective_component(entity) as ZC_Objective
+
+		if not objective:
+			continue
+
+		if objective.key == "":
+			printerr("Objective component on entity %s has an empty key!" % entity.name)
+			continue
+
+		if objective.key in objective_names:
+			print("Found objective %s in the level." % objective.key)
+		else:
+			missing += 1
+			printerr("Objective %s does not exist in the level!" % objective.key)
+
+	if missing == 0:
+		print("Level has all objectives.")
+	else:
+		printerr("Level is missing %d objectives!" % missing)
