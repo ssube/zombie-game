937cc029de5a5f8ccf2119e1c69668cf4be8e40d
Sean Sube <seansube@gmail.com>
2025-12-21 12:37:38 -0600

    add support for multiple audio players to audio action

--- scripts/nodes/actions/ZN_AudioAction.gd
+++ scripts/nodes/actions/ZN_AudioAction.gd
@@ -1,10 +1,17 @@
 extends ZN_BaseAction
 class_name ZN_AudioAction
 
-@export var audio_player: AudioStreamPlayer3D
+enum AudioMode {
+	ALL,
+	RANDOM,
+}
+
+@export var audio_mode: AudioMode = AudioMode.ALL
+@export var audio_players: Array[AudioStreamPlayer3D] = []
 
 func run_node(_source: Node, _event: Enums.ActionEvent, _actor: Node) -> void:
-	if audio_player is ZN_AudioSubtitle3D:
-		audio_player.play_subtitle()
-	else:
-		audio_player.play()
+	for audio_player in audio_players:
+		if audio_player is ZN_AudioSubtitle3D:
+			audio_player.play_subtitle()
+		else:
+			audio_player.play()
