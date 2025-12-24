bb607b2b4514e5878c33d07304f86edd1abc019d
Sean Sube <seansube@gmail.com>
2025-12-23 20:25:53 -0600

    add snap to nav mesh option to spawn action

--- scripts/nodes/actions/ZN_SpawnAction.gd
+++ scripts/nodes/actions/ZN_SpawnAction.gd
@@ -25,6 +25,7 @@ enum SpawnParent {
 @export var spawn_scenes: Array[PackedScene] = []
 @export var spawn_marker: Marker3D = null
 @export var random_spread: Vector3 = Vector3.ZERO
+@export var snap_to_nav_mesh: bool = true
 
 
 func run_node(source: Node, _event: Enums.ActionEvent, actor: Node) -> void:
@@ -64,12 +65,21 @@ func _get_position(actor: Node, source: Node) -> Vector3:
 	return node.global_position
 
 
-func _random_offset(scene: Node) -> void:
-	assert(scene is Node3D, "scene must be a 3D node to be offset!")
+func _random_offset(node: Node3D) -> Vector3:
+	assert(node is Node3D, "scene must be a 3D node to be offset!")
 
 	# TODO: spread both positive and negative direction
 	var offset := Vector3(randf() * random_spread.x, randf() * random_spread.y, randf() * random_spread.z)
-	scene.global_position += offset
+	return offset
+
+
+func _snap_to_nav_mesh(node: Node3D, position: Vector3) -> Vector3:
+	var default_map_rid: RID = node.get_world_3d().get_navigation_map()
+
+	return NavigationServer3D.map_get_closest_point(
+		default_map_rid,
+		position
+	)
 
 
 func _spawn_all(actor: Node, source: Node) -> void:
@@ -96,7 +106,10 @@ func _spawn_scene(scene: PackedScene, parent: Node, position: Vector3) -> void:
 	instance.global_position = position
 
 	if not is_zero_approx(random_spread.length_squared()):
-		_random_offset(instance)
+		instance.global_position += _random_offset(instance)
+
+	if snap_to_nav_mesh:
+		instance.global_position = _snap_to_nav_mesh(instance, instance.global_position)
 
 	if instance is Entity:
 		ECS.world.add_entity(instance)
