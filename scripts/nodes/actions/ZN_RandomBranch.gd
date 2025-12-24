c6ed50cd551a6b5e1c974a60ccbae901cf27ea3d
Sean Sube <seansube@gmail.com>
2025-12-24 13:43:43 -0600

    fix upper limit in random branch action

--- scripts/nodes/actions/ZN_RandomBranch.gd
+++ scripts/nodes/actions/ZN_RandomBranch.gd
@@ -2,5 +2,5 @@ extends ZN_BranchAction
 class_name ZN_RandomBranch
 
 func run_node(source: Node, event: Enums.ActionEvent, actor: Node) -> void:
-	var value := randi_range(0, _actions.size())
+	var value := randi_range(0, _actions.size() - 1)
 	_actions[value]._run(source, event, actor)
