98b1d81fc1927ea8d5d7bcf7ca805d0170301433
Sean Sube <seansube@gmail.com>
2025-12-25 09:18:18 -0600

    add ambient audio to spooky church level

---
+++ scripts/nodes/ZN_AudioLoopPlayer.gd
@@ -0,0 +1,27 @@
+extends AudioStreamPlayer
+class_name ZN_AudioLoopPlayer
+
+@export var loop_on_ready: bool = true
+@export var max_loops: int = -1
+
+var _loop_count: int = 0
+
+
+func _ready() -> void:
+	if max_loops > -1:
+		self.finished.connect(_loop)
+
+	if loop_on_ready:
+		self.play()
+
+
+func _loop() -> void:
+	if max_loops == -1:
+		self.play()
+		return
+
+	_loop_count += 1
+	if _loop_count >= max_loops:
+		return
+
+	self.play()
