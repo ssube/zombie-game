998e12bc1d8e10dc93e60f607dc778cf615ede7a
Sean Sube <seansube@gmail.com>
2025-12-26 18:10:19 -0600

    fix throttling of timers in zombie behavior

--- scripts/behavior/states/ZB_Idle.gd
+++ scripts/behavior/states/ZB_Idle.gd
@@ -15,7 +15,6 @@ func enter(entity: Entity):
 	look_direction = NavigationUtils.pick_random_position(node_3d, look_radius)
 
 func tick(entity: Entity, delta: float, _behavior: ZC_Behavior) -> TickResult:
-	# print("Zombie is idling.")
 	var entity3d := entity.get_node(".") as Node3D
 	var movement := entity.get_component(ZC_Movement) as ZC_Movement
 	movement.target_move_position = entity3d.global_position
