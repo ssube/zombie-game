206223dbeb40174fbca4d65a61fa0c8b38547690
Sean Sube <seansube@gmail.com>
2025-12-30 14:43:07 -0600

    implement pause between loops for path follower

--- scripts/nodes/ZN_PathFollow3D.gd
+++ scripts/nodes/ZN_PathFollow3D.gd
@@ -2,24 +2,27 @@ extends PathFollow3D
 ## Scriptable path follow node with automatic start, loop, and speed control.
 class_name ZN_PathFollow3D
 
+
 @export var start_on_ready: bool = true
 @export var reset_on_start: bool = true
 @export var duration: float = 5.0  # Time in seconds to complete one loop
+@export var pause_between_loops: float = 0.0
+@export var hide_between_loops: bool = true
 @export var reverse: bool = false
 @export var path: Path3D
-# TODO: add speed curve support
 
-var _time_passed: float = 0.0
+
 var _is_moving: bool = false
 var _tween: Tween
+
 
 func _ready() -> void:
 	if start_on_ready:
 		start()
 
+
 func start() -> void:
 	_is_moving = true
-	_time_passed = 0.0
 	_tween = create_tween()
 	if reverse:
 		if reset_on_start:
@@ -41,5 +44,12 @@ func stop() -> void:
 
 
 func _on_tween_completed() -> void:
+	if pause_between_loops > 0.0:
+		if hide_between_loops:
+			visible = false
+		await get_tree().create_timer(pause_between_loops).timeout
+		if hide_between_loops:
+			visible = true
+
 	if loop and _is_moving:
 		start()
