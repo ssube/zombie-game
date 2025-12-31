9fcb2af3873ff79d7543469829614a03e535a5ab
Sean Sube <seansube@gmail.com>
2025-12-31 13:15:23 -0600

    add some floating boxes to the fishing pier level, make the buoyancy script damp all motion

--- scripts/nodes/ZN_Buoyant.gd
+++ scripts/nodes/ZN_Buoyant.gd
@@ -15,11 +15,12 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	var height_diff: float = target_height - self.global_position.y
 	var buoyant_force: float = height_diff * buoyancy_strength
-	var vertical_velocity: float = self.linear_velocity.y
-	buoyant_force -= vertical_velocity * damping * delta
 	buoyant_force = clamp(buoyant_force, -max_velocity, max_velocity)
 
-	var damped_velocity: Vector3 = self.linear_velocity.lerp(Vector3.ZERO, damping * delta)
-	self.angular_velocity = damped_velocity
+	var damped_linear_velocity: Vector3 = self.linear_velocity.lerp(Vector3.ZERO, damping * delta)
+	self.linear_velocity = damped_linear_velocity
+
+	var damped_angular_velocity: Vector3 = self.angular_velocity.lerp(Vector3.ZERO, angular_damping * delta)
+	self.angular_velocity = damped_angular_velocity
 
 	self.apply_central_force(Vector3.UP * buoyant_force)
