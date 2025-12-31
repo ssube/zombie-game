e304d0a2d094b7cf0c0877822855c5f0bc17032a
Sean Sube <seansube@gmail.com>
2025-12-30 21:14:03 -0600

    split up damage and health calculations to clean up the health system

--- scripts/systems/ZS_Health.gd
+++ scripts/systems/ZS_Health.gd
@@ -8,8 +8,10 @@ enum HealthMode {
 
 @export var mode: HealthMode = HealthMode.LAST_DAMAGE
 
+
 func query():
 	return q.with_all([ZC_Health]).with_relationship([RelationshipUtils.any_damage])
+
 
 func process(entities: Array[Entity], _components: Array, _delta: float):
 	for entity in entities:
@@ -21,53 +23,10 @@ func process(entities: Array[Entity], _components: Array, _delta: float):
 		if damages.size() == 0:
 			continue
 
-		var total_damage := 0
-		for damage_rel in damages:
-			var c_damage := damage_rel.target as ZC_Damage
-			total_damage += floor(c_damage.amount)
-			entity.remove_relationship(damage_rel)
-
-		# Skip damage after removing relationships, if this is a player with god mode
-		if EntityUtils.is_player(entity) and OptionsManager.options.cheats.god_mode:
-			continue
-
-		var damage: int = total_damage
-
-		# Do not apply damage resistance to healing (negative damage)
-		if damage > 0:
-			var multiplier: float = EntityUtils.get_damage_multiplier(entity)
-			damage = floor(total_damage * multiplier)
-
 		var health := entity.get_component(ZC_Health) as ZC_Health
-		var previous_health := health.current_health
-		var adjusted_health := previous_health - damage
-
-		if damage > 0:
-			var damage_source := _calculate_damage_source(damages)
-			var hit_by := ECS.world.get_entity_by_id(damage_source)
-
-			if hit_by:
-				var hit := Relationship.new(ZC_Hit.new(), hit_by)
-				RelationshipUtils.add_unique_relationship(entity, hit)
-
-				# TODO: find a better way to only add this once per entity
-				# the observer already has both of these values, for example
-				if adjusted_health <= 0 and previous_health > 0:
-					var killed := Relationship.new(ZC_Killed.new(damage_source), entity)
-					hit_by.add_relationship(killed)
-
-			if EntityUtils.is_player(entity):
-				var effect_strength := 1.0 - (adjusted_health / float(health.max_health))
-				effect_strength /= 2.0
-
-				var effect := ZC_Screen_Effect.new()
-				effect.effect = ZM_BaseMenu.Effects.DAMAGE
-				effect.duration = effect_strength * 5.0
-				effect.strength = effect_strength
-				var effect_rel := RelationshipUtils.make_effect(effect)
-				entity.add_relationship(effect_rel)
-
-		health.current_health -= damage
+		_apply_damage(entity, health, damages)
+		_apply_healing(entity, health, damages)
+		entity.remove_relationships(damages)
 
 
 func _calculate_damage_source(damages: Array[Relationship]) -> String:
@@ -111,3 +70,62 @@ func _calculate_most_damage_source(damages: Array[Relationship]) -> String:
 			max_damage = amount
 
 	return max_source
+
+
+func _apply_damage(entity: Entity, health: ZC_Health, damages: Array[Relationship]) -> void:
+	if EntityUtils.is_player(entity) and OptionsManager.options.cheats.god_mode:
+		return
+
+	var total_damage := 0
+	for damage_rel in damages:
+		var c_damage := damage_rel.target as ZC_Damage
+		if c_damage.amount > 0:
+			total_damage += floor(c_damage.amount)
+
+	var real_damage = floori(total_damage * EntityUtils.get_damage_multiplier(entity))
+
+	if real_damage <= 0:
+		return
+
+	var previous_health := health.current_health
+	var adjusted_health := maxi(previous_health - total_damage, 0)
+
+	var damage_source := _calculate_damage_source(damages)
+	var hit_by := ECS.world.get_entity_by_id(damage_source)
+
+	if hit_by:
+		var hit := Relationship.new(ZC_Hit.new(), hit_by)
+		RelationshipUtils.add_unique_relationship(entity, hit)
+
+		# TODO: find a better way to only add this once per entity
+		# the observer already has both of these values, for example
+		if adjusted_health <= 0 and previous_health > 0:
+			var killed := Relationship.new(ZC_Killed.new(damage_source), entity)
+			hit_by.add_relationship(killed)
+
+	if EntityUtils.is_player(entity):
+		var effect_strength := 1.0 - (adjusted_health / float(health.max_health))
+		effect_strength /= 2.0
+
+		var effect := ZC_Screen_Effect.new()
+		effect.effect = ZM_BaseMenu.Effects.DAMAGE
+		effect.duration = effect_strength * 5.0
+		effect.strength = effect_strength
+		var effect_rel := RelationshipUtils.make_effect(effect)
+		entity.add_relationship(effect_rel)
+
+	# Should happen as the last step, once all of the relationships are in place
+	health.current_health -= real_damage
+
+
+func _apply_healing(_entity: Entity, health: ZC_Health, damages: Array[Relationship]) -> void:
+	var total_healing := 0
+	for damage_rel in damages:
+		var c_damage := damage_rel.target as ZC_Damage
+		if c_damage.amount < 0:
+			total_healing += ceili( -c_damage.amount )
+
+	if total_healing <= 0:
+		return
+
+	health.current_health += total_healing
