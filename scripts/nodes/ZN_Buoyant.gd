29c1452ce7f0c0265562f70bb5e39e69055f848e
Sean Sube <seansube@gmail.com>
2025-12-31 12:37:52 -0600

    add bouyancy script for rigid bodies, apply to the floating monsters in the spooky church level

---
+++ scripts/nodes/ZN_Buoyant.gd
@@ -0,0 +1,25 @@
+extends RigidBody3D
+class_name ZN_Buoyant
+
+@export var buoyancy_strength: float = 10
+@export var target_height: float = 0.0
+@export var use_starting_height: bool = true
+@export var damping: float = 1
+@export var angular_damping: float = 1
+@export var max_velocity: float = 10.0
+
+func _ready() -> void:
+	if use_starting_height:
+		target_height = self.global_position.y
+
+func _physics_process(delta: float) -> void:
+	var height_diff: float = target_height - self.global_position.y
+	var buoyant_force: float = height_diff * buoyancy_strength
+	var vertical_velocity: float = self.linear_velocity.y
+	buoyant_force -= vertical_velocity * damping * delta
+	buoyant_force = clamp(buoyant_force, -max_velocity, max_velocity)
+
+	var damped_velocity: Vector3 = self.linear_velocity.lerp(Vector3.ZERO, damping * delta)
+	self.angular_velocity = damped_velocity
+
+	self.apply_central_force(Vector3.UP * buoyant_force)
