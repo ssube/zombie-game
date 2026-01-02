bb6b1b6b934fa621cde1660ed3ff7adf38e7a4e8
Sean Sube <seansube@gmail.com>
2026-01-01 22:42:26 -0600

    add an option to make the fish swim more vertically

--- scripts/behavior/states/ZB_Wander_Swim.gd
+++ scripts/behavior/states/ZB_Wander_Swim.gd
@@ -4,16 +4,18 @@ class_name ZB_State_Wander_Swim
 ## Minimum swim height offset above navmesh (ocean floor)
 @export var swim_height_min: float = 1.0
 
-## Node3D to use for initial position reference (sets swim_height_max_absolute)
+@export var prefer_height_difference: bool = false
+
+## Node3D to use for initial position reference (sets swim_height_max)
 @export var initial_position: Node3D
 
 var spawn_height: float = 0.0
-var swim_height_max_absolute: float = 0.0
+var swim_height_max: float = 0.0
 
 
 func _ready() -> void:
 	if initial_position != null:
-		swim_height_max_absolute = initial_position.global_position.y
+		swim_height_max = initial_position.global_position.y
 
 
 func enter(entity: Entity) -> void:
@@ -29,10 +31,21 @@ func update_wander_target(entity) -> void:
 	# Calculate valid swim range: min height above floor to spawn height (or absolute max)
 	var navmesh_y := random_pos.y
 	var min_swim_y := navmesh_y + swim_height_min
-	var max_swim_y := swim_height_max_absolute
+	var max_swim_y := swim_height_max
 
-	# Pick a random height within the valid range
-	var target_y := randf_range(min_swim_y, max_swim_y)
+	var target_y := navmesh_y
+	if prefer_height_difference:
+		# Find the furthest point in the valid range from current height
+		var current_y := entity.root_3d.global_position.y as float
+		var dist_to_min := absf(current_y - min_swim_y)
+		var dist_to_max := absf(current_y - max_swim_y)
+		if dist_to_min > dist_to_max:
+			target_y = randf_range(min_swim_y, current_y)
+		else:
+			target_y = randf_range(current_y, max_swim_y)
+	else:
+		# Pick a random height within the valid range
+		target_y = randf_range(min_swim_y, max_swim_y)
 
 	target_position = Vector3(random_pos.x, target_y, random_pos.z)
 
