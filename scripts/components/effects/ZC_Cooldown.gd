0b852a42f8628611b7f3828d7a00f94d05e831d3
Sean Sube <seansube@gmail.com>
2026-01-04 16:14:55 -0600

    initialize cooldown to a valid value

--- scripts/components/effects/ZC_Cooldown.gd
+++ scripts/components/effects/ZC_Cooldown.gd
@@ -3,6 +3,7 @@ class_name ZC_Cooldown
 
 @export var time_remaining: float = 0.0
 
-func _init(time: float = 0.0):
+
+func _init(time: float = 1.0):
 	assert(time > 0, "Cooldown time should be > 0!")
 	time_remaining = time
