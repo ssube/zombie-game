379b6fa48aa13d64f88cc9e98c38c5a35ac872ef
Sean Sube <seansube@gmail.com>
2026-01-04 16:13:27 -0600

    make tab container with audio bind itself

--- menus/tab_container.gd
+++ menus/tab_container.gd
@@ -2,6 +2,11 @@ extends TabContainer
 
 @export var audio_player: AudioStreamPlayer = null
 
+
+func _ready() -> void:
+	self.tab_clicked.connect(_on_tab_clicked)
+
+
 func _on_tab_clicked(_tab: int) -> void:
 	if audio_player:
 		audio_player.play()
