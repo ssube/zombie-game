d0e4b0f6f42427a2b63440c44580bb2bce475902
Sean Sube <seansube@gmail.com>
2026-01-10 16:02:25 -0600

    add a component for books

---
+++ scripts/components/props/ZC_Book.gd
@@ -0,0 +1,9 @@
+extends Component
+class_name ZC_Book
+
+@export var title: String = "Book Title"
+@export_multiline var pages: Array[String] = [
+	"Page 1: This is the first page of the book.",
+	"Page 2: This is the second page of the book.",
+	"Page 3: This is the third page of the book.",
+]
--- scripts/menus/props/book.gd
+++ scripts/menus/props/book.gd
@@ -1,23 +1,12 @@
 extends ZM_BaseMenu
 
-
-class TestBook:
-	var title: String = "The Great Book"
-	var pages: Array[String] = [
-		"Page 1: Once upon a time...",
-		"Page 2: In a land far, far away...",
-		"Page 3: There lived a brave hero.",
-	]
-
-
 @export var book_title: Label
 @export var book_text: RichTextLabel
 @export var page_label: Label
-
-@onready var page_template: String = page_label.text
+@export var page_template: String = "Page %d of %d"
 
 var current_page: int = 0
-var book: TestBook = TestBook.new()
+var current_book: ZC_Book = null
 
 
 func _on_close_button_pressed() -> void:
@@ -25,8 +14,20 @@ func _on_close_button_pressed() -> void:
 
 
 func on_update() -> void:
-	book_text.text = book.pages[current_page]
-	page_label.text = page_template % [current_page + 1, book.pages.size()]
+	var pages := current_book.pages
+	book_text.text = pages[current_page]
+	page_label.text = page_template % [current_page + 1, pages.size()]
+
+
+func set_data(data: Dictionary) -> void:
+	var source := data.get("source", null) as Entity
+	var c_book := source.get_component(ZC_Book) as ZC_Book
+	if c_book == null:
+		return
+
+	current_book = c_book
+	current_page = 0
+	on_update()
 
 
 func _on_previous_button_pressed() -> void:
@@ -35,5 +36,15 @@ func _on_previous_button_pressed() -> void:
 
 
 func _on_next_button_pressed() -> void:
-	current_page = mini(current_page + 1, book.pages.size() - 1)
+	current_page = mini(current_page + 1, current_book.pages.size() - 1)
+	on_update()
+
+
+func _on_first_button_pressed() -> void:
+	current_page = 0
+	on_update()
+
+
+func _on_last_button_pressed() -> void:
+	current_page = current_book.pages.size() - 1
 	on_update()
