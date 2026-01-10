759346065de5d910389cd4101a5938c127875dd6
Sean Sube <seansube@gmail.com>
2026-01-09 19:01:16 -0600

    reformat enum

--- scripts/resources/ZR_Weapon_Effect.gd
+++ scripts/resources/ZR_Weapon_Effect.gd
@@ -1,7 +1,16 @@
 extends Resource
 class_name ZR_Weapon_Effect
 
-enum EffectType { CODE_ONLY, MELEE_SWING, RANGED_FIRE, RANGED_RECOIL, RANGED_RELOAD, MELEE_BREAK, MELEE_REPAIR, RANGED_EMPTY }
+enum EffectType {
+	CODE_ONLY,
+	MELEE_SWING,
+	RANGED_FIRE,
+	RANGED_RECOIL,
+	RANGED_RELOAD,
+	MELEE_BREAK,
+	MELEE_REPAIR,
+	RANGED_EMPTY,
+}
 
 @export var effect_type: EffectType = EffectType.CODE_ONLY
 @export var marker: NodePath
