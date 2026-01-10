e4712722f0d299640c41f936396756d313042082
Sean Sube <seansube@gmail.com>
2026-01-10 16:37:24 -0600

    add an option to the timer action to stop timers, use it to stop the zombie sounds when they die

--- scripts/nodes/actions/ZN_TimerAction.gd
+++ scripts/nodes/actions/ZN_TimerAction.gd
@@ -2,8 +2,12 @@ extends ZN_BaseAction
 class_name ZN_TimerAction
 
 @export var start: bool = true
+@export var stop: bool = false
 @export var timer: Timer = null
 
 func run_node(_source: Node, _event: Enums.ActionEvent, _actor: Node) -> void:
 	if start:
 		timer.start()
+
+	if stop:
+		timer.stop()
