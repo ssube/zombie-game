9bac83c91af54df3f168423a2f304b9437ad29a3
Sean Sube <seansube@gmail.com>
2026-01-13 22:26:52 -0600

    add a debug console command that prints info about an entity

--- scripts/menus/console_menu.gd
+++ scripts/menus/console_menu.gd
@@ -11,6 +11,35 @@ signal command_submitted(command: String)
 var _history: Array[String] = []
 
 
+func _run_command_debug(target_id: String):
+	_history.append("Debugging entity %s..." % [target_id])
+
+	var target := ECS.world.get_entity_by_id(target_id)
+	if target == null:
+		_history.append("Entity %s not found!" % [target_id])
+		return
+
+	_history.append("Scene tree path: %s" % [str(target.get_path())])
+
+	# 3D data
+	var target_node := target as Node
+	if target_node is Node3D:
+		var target_3d := target_node as Node3D
+		_history.append("Position: %s" % [str(target_3d.global_position)])
+		_history.append("Rotation: %s" % [str(target_3d.global_rotation)])
+		_history.append("Scale: %s" % [str(target_3d.global_scale)])
+
+	if target_node is RigidBody3D:
+		var target_rigid := target_node as RigidBody3D
+		_history.append("Linear Velocity: %s" % [str(target_rigid.linear_velocity)])
+		_history.append("Angular Velocity: %s" % [str(target_rigid.angular_velocity)])
+
+	# ECS data
+	_history.append("Components:")
+	for component in target.components:
+		_history.append(" - %s" % [component])
+
+
 func _run_command_give(target_id: String, item_path: String, quantity: int = 1):
 		_history.append("Giving %d of %s to %s..." % [quantity, item_path, target_id])
 
@@ -41,6 +70,17 @@ func _run_command_give(target_id: String, item_path: String, quantity: int = 1):
 		_history.append("Gave items successfully.")
 
 
+func _run_command_help():
+	_history.append("Available commands:")
+	_history.append(" clear")
+	_history.append(" debug <target_id>")
+	_history.append(" give <target_id> <item_path> [quantity]")
+	_history.append(" help")
+	_history.append(" load <level_name> [spawn_point]")
+	_history.append(" menu <menu_index>")
+	_history.append(" spawn <entity_path>")
+
+
 func _run_command_load(level: String, spawn: String = "Markers/Start"):
 	_history.append("Loading level %s at spawn %s..." % [level, spawn])
 	var game := TreeUtils.get_game(self)
@@ -66,6 +106,9 @@ func _run_command(text: String) -> void:
 	match keyword:
 		"clear":
 			_history.clear()
+		"debug":
+			var target_id := words[1]
+			_run_command_debug(target_id)
 		"give":
 			var target_id := words[1]
 			var item_path := words[2]
@@ -75,11 +118,7 @@ func _run_command(text: String) -> void:
 
 			_run_command_give(target_id, item_path, quantity)
 		"help":
-			_history.append("Available commands:")
-			_history.append(" give <target_id> <item_path> [quantity]")
-			_history.append(" load <level_name> [spawn_point]")
-			_history.append(" menu <menu_index>")
-			_history.append(" spawn <entity_path>")
+			_run_command_help()
 		"load":
 			var level := words[1]
 			var spawn := "Markers/Start"
@@ -96,17 +135,18 @@ func _run_command(text: String) -> void:
 
 
 func _on_console_input_text_submitted(text: String) -> void:
+	command_input.text = ""
+	_history.append("> %s" % [text])
 	command_submitted.emit(text)
 	_run_command(text)
 
-	_history.append(text)
-	command_input.text = ""
 	on_update()
+	command_input.grab_focus.call_deferred()
 
 
 func on_show() -> void:
 	super.on_show()
-	command_input.grab_focus()
+	command_input.grab_focus.call_deferred()
 
 
 func on_update() -> void:
