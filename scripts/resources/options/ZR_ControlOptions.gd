f7ba8bbbcb912914ea66b2a3d2cf990b936213ee
Sean Sube <seansube@gmail.com>
2026-01-16 20:34:52 -0600

    increase default mouse sensitivity

--- scripts/resources/options/ZR_ControlOptions.gd
+++ scripts/resources/options/ZR_ControlOptions.gd
@@ -1,6 +1,6 @@
 extends Resource
 class_name ZR_ControlOptions
 
-@export var mouse_sensitivity: int = 50
+@export var mouse_sensitivity: int = 75
 #@export var controller_left_sensitivity: float = 1.0
 #@export var controller_right_sensitivity: float = 1.0
